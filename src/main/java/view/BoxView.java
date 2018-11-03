81c471b5d5b60746a7967b339af0b4761d9c9a85
eulerscheZahl <eulerschezahl@yandex.com>
2018-11-03 11:04:33 +0100

    use garbage ball

--- src/main/java/view/BoxView.java
+++ src/main/java/view/BoxView.java
@@ -1,6 +1,7 @@
 package view;
 
 import Bender4.Box;
+import Bender4.Cell;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
 import com.codingame.gameengine.module.entities.Group;
 import com.codingame.gameengine.module.entities.Sprite;
@@ -8,21 +9,28 @@ import com.codingame.gameengine.module.entities.Sprite;
 public class BoxView {
     private Box box;
     private Sprite sprite;
-    private static final int rows = 12;
+    private Cell location;
 
     public BoxView(Group boardGroup, Box box, GraphicEntityModule graphics) {
         this.box = box;
         box.view = this;
+        location = box.cell;
 
         sprite = graphics.createSprite()
-                .setX(box.cell.x * BoardView.CELL_SIZE)
-                .setY(box.cell.y * BoardView.CELL_SIZE);
-        sprite.setImage("box.png");
+                .setX(box.cell.x * BoardView.CELL_SIZE + BoardView.CELL_SIZE / 2)
+                .setY(box.cell.y * BoardView.CELL_SIZE + BoardView.CELL_SIZE / 2)
+                .setAnchor(0.5);
+        sprite.setImage("Garbage Ball.png");
         boardGroup.add(sprite);
     }
 
     public void update() {
-        sprite.setX(box.cell.x * BoardView.CELL_SIZE)
-                .setY(box.cell.y * BoardView.CELL_SIZE);
+        if (location.x < box.cell.x) sprite.setRotation(sprite.getRotation() + Math.PI / 2);
+        if (location.x > box.cell.x) sprite.setRotation(sprite.getRotation() - Math.PI / 2);
+
+        sprite.setX(box.cell.x * BoardView.CELL_SIZE + BoardView.CELL_SIZE / 2)
+                .setY(box.cell.y * BoardView.CELL_SIZE + BoardView.CELL_SIZE / 2);
+
+        location = box.cell;
     }
 }
