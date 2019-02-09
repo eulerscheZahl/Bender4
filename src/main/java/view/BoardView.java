d4e7cd09a72b48af7d7491880c52841d1280d983
eulerscheZahl <eulerschezahl@yandex.com>
2019-02-09 21:38:37 +0100

    updated to v3.4.1, TODO: review last frame

--- src/main/java/view/BoardView.java
+++ src/main/java/view/BoardView.java
@@ -1,79 +1,86 @@
 package view;
 
-import Bender4.Board;
-import Bender4.Cell;
-import com.codingame.game.Referee;
-import com.codingame.gameengine.module.entities.*;
-import modules.TooltipModule;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import com.codingame.game.Referee;
+import com.codingame.gameengine.module.entities.BufferedGroup;
+import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.codingame.gameengine.module.entities.Group;
+import com.codingame.gameengine.module.entities.Sprite;
+import com.codingame.gameengine.module.entities.SpriteAnimation;
+
+import Bender4.Board;
+import Bender4.Cell;
+import modules.TooltipModule;
+
 public class BoardView {
-    private GraphicEntityModule graphics;
-    public static final int CELL_SIZE = 48;
-    public Group boardGroup;
-    public Group functionsGroup;
-    private static String[] wallSprites;
-    private SpriteAnimation fry;
+	private GraphicEntityModule graphics;
+	public static final int CELL_SIZE = 48;
+	public Group boardGroup;
+	public Group functionsGroup;
+	private static String[] wallSprites;
+	private SpriteAnimation fry;
 
-    public BoardView(Board board, GraphicEntityModule graphics, TooltipModule tooltip) {
-        wallSprites = Utils.loadSheet(graphics, "walls.png", CELL_SIZE, CELL_SIZE, 16, 4);
-        this.graphics = graphics;
-        tooltip.setSize(board.width);
+	public BoardView(Board board, GraphicEntityModule graphics, TooltipModule tooltip) {
+		wallSprites = Utils.loadSheet(graphics, "walls.png", CELL_SIZE, CELL_SIZE, 16, 4);
+		this.graphics = graphics;
+		tooltip.setSize(board.width);
 
-        boardGroup = graphics.createGroup();
-        boardGroup.setScale(1080.0 / (board.height * CELL_SIZE));
-        functionsGroup = graphics.createGroup().setX(1120);
-        BufferedGroup gridGroup = graphics.createBufferedGroup();
-        boardGroup.add(gridGroup);
-        Group innerGroup = graphics.createGroup();
-        gridGroup.add(innerGroup);
+		boardGroup = graphics.createGroup();
+		boardGroup.setScale(1080.0 / (board.height * CELL_SIZE));
+		functionsGroup = graphics.createGroup().setX(1120);
+		BufferedGroup gridGroup = graphics.createBufferedGroup();
+		boardGroup.add(gridGroup);
+		Group innerGroup = graphics.createGroup();
+		gridGroup.add(innerGroup);
 
-        Sprite background = graphics.createSprite().setImage("frame.png").setX(-40).setZIndex(-1);
-        Sprite backgroundTop = graphics.createSprite().setImage("frameTop.png").setX(-40).setZIndex(3);
+		Sprite background = graphics.createSprite().setImage("frame.png").setX(-40).setZIndex(-1);
+		Sprite backgroundTop = graphics.createSprite().setImage("frameTop.png").setX(-40).setZIndex(3);
 
-        Sprite zoidberg = graphics.createSprite().setImage("Zoidberg.png").setX(55).setY(130).setAlpha(0.05).setZIndex(3);
-        functionsGroup.add(background);
-        functionsGroup.add(backgroundTop);
-        functionsGroup.add(zoidberg);
-        Sprite floor = Utils.createBoardSprite(graphics, "fullFloor.png", 0, 0).setZIndex(-2);
-        innerGroup.add(floor);
+		Sprite zoidberg = graphics.createSprite().setImage("Zoidberg.png").setX(55).setY(130).setAlpha(0.05).setZIndex(3);
+		functionsGroup.add(background);
+		functionsGroup.add(backgroundTop);
+		functionsGroup.add(zoidberg);
+		Sprite floor = Utils.createBoardSprite(graphics, "fullFloor.png", 0, 0).setZIndex(-2);
+		innerGroup.add(floor);
 
-        for (int x = 0; x < board.width; x++) {
-            for (int y = 0; y < board.height; y++) {
-                if (board.grid[x][y].isWall) {
-                   Sprite wall = Utils.createBoardSprite(graphics, selectWall(board.grid, board.width, board.height, x, y), x, y);
-                   innerGroup.add(wall);
-                }
-            }
-        }
+		for (int x = 0; x < board.width; x++) {
+			for (int y = 0; y < board.height; y++) {
+				if (board.grid[x][y].isWall) {
+					Sprite wall = Utils.createBoardSprite(graphics, selectWall(board.grid, board.width, board.height, x, y), x, y);
+					innerGroup.add(wall);
+				}
+			}
+		}
 
-        String[] frySheet = Utils.loadSheet(graphics, "Fry.png", CELL_SIZE, CELL_SIZE, 2, 1);
-        fry = Utils.createAnimation(graphics, frySheet)
-                .setX(board.target.x * CELL_SIZE)
-                .setY(board.target.y * CELL_SIZE);
-        boardGroup.add(fry);
-        Map<String, Object> params = new HashMap<>();
-        params.put("Type", "Fry");
-        tooltip.registerEntity(fry, params);
-    }
+		String[] frySheet = Utils.loadSheet(graphics, "Fry.png", CELL_SIZE, CELL_SIZE, 2, 1);
+		fry = Utils.createAnimation(graphics, frySheet).setX(board.target.x * CELL_SIZE).setY(board.target.y * CELL_SIZE);
+		boardGroup.add(fry);
+		Map<String, Object> params = new HashMap<>();
+		params.put("Type", "Fry");
+		tooltip.registerEntity(fry, params);
+	}
 
-    public void win() {
-        String[] victorySheet = Utils.loadSheet(graphics, "FryVictory.png", BoardView.CELL_SIZE, BoardView.CELL_SIZE, 5, 5);
-        fry.setImages(victorySheet).setDuration(Referee.FRAME_DURATION * 2).stop().setLoop(false);
-        fry.setX(fry.getX() - 5);
-        graphics.commitEntityState(0.101, fry);
-        fry.start();
-        graphics.commitEntityState(0.102, fry);
-    }
+	public void win() {
+		String[] victorySheet = Utils.loadSheet(graphics, "FryVictory.png", BoardView.CELL_SIZE, BoardView.CELL_SIZE, 5, 5);
+		fry.setImages(victorySheet).setDuration(Referee.FRAME_DURATION * 2).pause().setLoop(false);
+		fry.setX(fry.getX() - 5);
+		graphics.commitEntityState(0.101, fry);
+		fry.play();
+		graphics.commitEntityState(0.102, fry);
+	}
 
-    private static String selectWall(Cell[][] grid, int width, int height, int x, int y) {
-        int index = 0;
-        if (y > 0 && grid[x][y - 1].isWall) index += 1;
-        if (y + 1 < height && grid[x][y + 1].isWall) index += 2;
-        if (x > 0 && grid[x - 1][y].isWall) index += 8;
-        if (x + 1 < width && grid[x + 1][y].isWall) index += 4;
-        return wallSprites[index];
-    }
+	private static String selectWall(Cell[][] grid, int width, int height, int x, int y) {
+		int index = 0;
+		if (y > 0 && grid[x][y - 1].isWall)
+			index += 1;
+		if (y + 1 < height && grid[x][y + 1].isWall)
+			index += 2;
+		if (x > 0 && grid[x - 1][y].isWall)
+			index += 8;
+		if (x + 1 < width && grid[x + 1][y].isWall)
+			index += 4;
+		return wallSprites[index];
+	}
 }
--- src/main/java/view/FunctionView.java
+++ src/main/java/view/FunctionView.java
@@ -1,100 +1,108 @@
 package view;
 
-import Bender4.Interpreter;
-import com.codingame.game.Referee;
+import java.util.LinkedList;
+
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
 import com.codingame.gameengine.module.entities.Group;
 import com.codingame.gameengine.module.entities.Sprite;
 import com.codingame.gameengine.module.entities.SpriteAnimation;
 
-import java.util.LinkedList;
+import Bender4.Interpreter;
 
 public class FunctionView {
-    private LinkedList<Sprite> actions = new LinkedList<>();
-    private Group functionGroup;
-    private Group bodyGroup;
-    private Interpreter.FunctionExecution function;
-    private GraphicEntityModule graphics;
+	private LinkedList<Sprite> actions = new LinkedList<>();
+	private Group functionGroup;
+	private Group bodyGroup;
+	private Interpreter.FunctionExecution function;
+	private GraphicEntityModule graphics;
 
-    private static String[] spriteSheet;
-    private static String[] functionSheet;
-    private static final int ACTIONS_SHOWN = 11;
-    private int offset;
-    private static final int IMAGE_SIZE = 50;
-    private SpriteAnimation frame;
+	private static String[] spriteSheet;
+	private static String[] functionSheet;
+	private static final int ACTIONS_SHOWN = 11;
+	private int offset;
+	private static final int IMAGE_SIZE = 50;
+	private SpriteAnimation frame;
 
-    public FunctionView(Interpreter.FunctionExecution functionExecution, int depth, int name, Group functionsGroup, GraphicEntityModule graphics) {
-        if (depth >= 7) return; // most likely infinite recursion, don't show that
-        this.graphics = graphics;
-        this.function = functionExecution;
-        spriteSheet = Utils.loadSheet(graphics, "functions.png", IMAGE_SIZE, IMAGE_SIZE, 14, 14);
-        functionSheet = Utils.loadSheet(graphics, "functionbox.png", 630, 88, 2, 1);
+	public FunctionView(Interpreter.FunctionExecution functionExecution, int depth, int name, Group functionsGroup, GraphicEntityModule graphics) {
+		if (depth >= 7)
+			return; // most likely infinite recursion, don't show that
+		this.graphics = graphics;
+		this.function = functionExecution;
+		spriteSheet = Utils.loadSheet(graphics, "functions.png", IMAGE_SIZE, IMAGE_SIZE, 14, 14);
+		functionSheet = Utils.loadSheet(graphics, "functionbox.png", 630, 88, 2, 1);
 
-        frame = Utils.createAnimation(graphics, functionSheet).setX(50).setY(80 + 100 * depth).setZIndex(1);
-        functionsGroup.add(frame);
-        functionGroup = graphics.createGroup().setX(80).setY(100 + 100 * depth);
-        functionsGroup.add(functionGroup);
-        bodyGroup = graphics.createGroup().setX(100);
-        functionGroup.add(bodyGroup);
+		frame = Utils.createAnimation(graphics, functionSheet).setX(50).setY(80 + 100 * depth).setZIndex(1);
+		functionsGroup.add(frame);
+		functionGroup = graphics.createGroup().setX(80).setY(100 + 100 * depth);
+		functionsGroup.add(functionGroup);
+		bodyGroup = graphics.createGroup().setX(100);
+		functionGroup.add(bodyGroup);
 
-        Sprite nameSprite = graphics.createSprite().setImage(spriteSheet[name]).setX(-1).setY(-1);
-        functionGroup.add(nameSprite);
+		Sprite nameSprite = graphics.createSprite().setImage(spriteSheet[name]).setX(-1).setY(-1);
+		functionGroup.add(nameSprite);
 
-        while (actions.size() < ACTIONS_SHOWN && offset < function.function.length()) {
-            Sprite sprite = graphics.createSprite();
-            setImage(sprite, function.function.charAt(offset));
-            sprite.setX((IMAGE_SIZE + 2) * offset - 1).setY(-1);
-            bodyGroup.add(sprite);
-            actions.add(sprite);
-            offset++;
-        }
-    }
+		while (actions.size() < ACTIONS_SHOWN && offset < function.function.length()) {
+			Sprite sprite = graphics.createSprite();
+			setImage(sprite, function.function.charAt(offset));
+			sprite.setX((IMAGE_SIZE + 2) * offset - 1).setY(-1);
+			bodyGroup.add(sprite);
+			actions.add(sprite);
+			offset++;
+		}
+	}
 
-    private void setImage(Sprite sprite, char c) {
-        if (c >= '1' && c <= '9') sprite.setImage(spriteSheet[c - '0']);
-        if (c == 'U') sprite.setImage(spriteSheet[10]);
-        if (c == 'L') sprite.setImage(spriteSheet[11]);
-        if (c == 'D') sprite.setImage(spriteSheet[12]);
-        if (c == 'R') sprite.setImage(spriteSheet[13]);
-    }
+	private void setImage(Sprite sprite, char c) {
+		if (c >= '1' && c <= '9')
+			sprite.setImage(spriteSheet[c - '0']);
+		if (c == 'U')
+			sprite.setImage(spriteSheet[10]);
+		if (c == 'L')
+			sprite.setImage(spriteSheet[11]);
+		if (c == 'D')
+			sprite.setImage(spriteSheet[12]);
+		if (c == 'R')
+			sprite.setImage(spriteSheet[13]);
+	}
 
-    public boolean delay = false;
+	public boolean delay = false;
 
-    public void step() {
-        if (delay) {
-            delay = false;
-            return;
-        }
-        if (bodyGroup == null) return;
-        bodyGroup.setX(bodyGroup.getX() - (IMAGE_SIZE + 2));
-        Sprite action = actions.pollFirst();
-        if (offset >= function.function.length()) {
-            action.setAlpha(0);
-        } else {
-            action.setX((IMAGE_SIZE + 2) * offset - 1);
-            graphics.commitEntityState(0, action);
-            setImage(action, function.function.charAt(offset));
-        }
-        actions.addLast(action);
-        offset++;
-    }
+	public void step() {
+		if (delay) {
+			delay = false;
+			return;
+		}
+		if (bodyGroup == null)
+			return;
+		bodyGroup.setX(bodyGroup.getX() - (IMAGE_SIZE + 2));
+		Sprite action = actions.pollFirst();
+		if (offset >= function.function.length()) {
+			action.setAlpha(0);
+		} else {
+			action.setX((IMAGE_SIZE + 2) * offset - 1);
+			graphics.commitEntityState(0, action);
+			setImage(action, function.function.charAt(offset));
+		}
+		actions.addLast(action);
+		offset++;
+	}
 
-    public void pause() {
-        if (frame != null) frame.stop();
-    }
+	public void pause() {
+		if (frame != null)
+			frame.pause();
+	}
 
-    public void resume() {
-        if (frame != null) {
-            frame.start();
-            graphics.commitEntityState(0, frame);
-        }
-    }
+	public void resume() {
+		if (frame != null) {
+			frame.play();
+			graphics.commitEntityState(0, frame);
+		}
+	}
 
-    public void terminate() {
-        functionGroup.setAlpha(0);
-        if (frame != null) {
-            frame.setAlpha(0);
-            graphics.commitEntityState(0, frame);
-        }
-    }
+	public void terminate() {
+		functionGroup.setAlpha(0);
+		if (frame != null) {
+			frame.setAlpha(0);
+			graphics.commitEntityState(0, frame);
+		}
+	}
 }
